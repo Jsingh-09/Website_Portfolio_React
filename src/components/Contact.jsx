aceb71a070b532e762f6c5d7f07969b1b6fb2ac3
Jashan Bhinder <jashan@jashans-air.lan>
2023-06-08 16:09:10 -0700

    Added resume link on home page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,9 +11,9 @@ const Contact = () => {
         </div>
         {/*<p className='text-gray-300'>Submit the form below:</p>*/}
         <form method='POST' action='https://getform.io/f/c24b0da2-804f-4287-a896-4e5ff9a27b64' className='flex flex-col'>
-          <input className='bg-[#ccd6f6] p-2' type='text' placeholder='Name' name='name' />
-          <input className='my-4 p-2 bg-[#ccd6f6]' type='email' placeholder='Email' name='email' />
-          <textarea className='bg-[#ccd6f6] p-2' name='message' rows='10' placeholder='Message'></textarea>
+          <input className='bg-white p-2' type='text' placeholder='Name' name='name' />
+          <input className='my-4 p-2 bg-white' type='email' placeholder='Email' name='email' />
+          <textarea className='bg-white p-2' name='message' rows='10' placeholder='Message'></textarea>
           <button className='text-white border-2 hover:bg-[#6fd2fa] hover:border-[#6fd2fa] px-4 py-3 my-8 mx-auto flex items-center'>
             SEND MESSAGE
           </button>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import ResumePDF from '../assets/Jashandeep_Singh_Resume_2023.pdf';
 
 const Home = () => {
-  return (
-    <div name= 'home' className= 'w-full h-screen bg-[#0a192f]'>
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-[#6fd2fa] text-2xl'>Hi! my name is </p>
-            <h1 className='text-5xl sm:text-7xl font-bold text-white'>Jashandeep Singh</h1>
-            <h2 className='text-5xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Software Engineer at Northrop Grumman.</h2>
-            <p className='text-[#8892b0] text-1.5xl py-4 max-w-[700px]'>I have my Bachelors in Computer Science from California State University, Stanislaus.</p>
-            <a href='https://github.com/Jsingh-09'>
-                <div>
-                    <button className='text-white font-bold border-2 px-6 py-3 flex items-center hover:bg-[#6fd2fa] hover:border-[#6fd2fa]'>View Work <HiArrowNarrowRight className='ml-3 hover:' /> </button>
-                </div>
-            </a>
-        </div>
-    </div>
-  )
-}
+  const openResume = () => {
+    window.open(ResumePDF, '_blank');
+  };
 
-export default Home
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-[#6fd2fa] text-2xl'>Hi! my name is</p>
+        <h1 className='text-5xl sm:text-7xl font-bold text-white'>Jashandeep Singh</h1>
+        <h2 className='text-5xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Software Engineer at Northrop Grumman.</h2>
+        <p className='text-[#8892b0] text-1.5xl py-4 max-w-[700px]'>I have my Bachelors in Computer Science from California State University, Stanislaus.</p>
+        <div>
+          <button className='text-white font-bold border-2 px-6 py-3 flex items-center hover:bg-[#6fd2fa] hover:border-[#6fd2fa]' onClick={openResume}>
+            View Resume <HiArrowNarrowRight className='ml-3 hover:' />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
